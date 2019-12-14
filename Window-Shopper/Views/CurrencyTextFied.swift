bf14c7ff0891014505bb21ecf61da507cab6278b
Israr <ahh.devoders@gmail.com>
2019-12-14 21:41:18 +0500

    Lecture 1 completed

---
+++ Window-Shopper/Views/CurrencyTextFied.swift
@@ -0,0 +1,29 @@
+//
+//  CurrencyTextFied.swift
+//  Window-Shopper
+//
+//  Created by Usama Sadiq on 12/14/19.
+//  Copyright © 2019 Usama Sadiq. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTextFied: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2984803082)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+
+        if let ph = placeholder
+        {
+            let place = NSAttributedString(string: ph, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) ])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+        
+    }
+
+}
+//init(string str: String, attributes attrs: [NSAttributedString.Key : Any]? = nil)
